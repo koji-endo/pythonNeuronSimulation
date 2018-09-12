0b1f920528e6a7f7e55ce5fb25db98ac28c65886
Hayato-t <tsunoda.gameobra@x.gmobb.jp>
2018-09-12 15:36:16 +0900

    multigraphplotter

--- util/multiGraphPlotter.py
+++ util/multiGraphPlotter.py
@@ -6,22 +6,38 @@ sys.path.append("./modules")
 import argparse
 import pickle
 
+def walk_files_with(extension, directory='.'):
+    """Generate paths of all files that has specific extension in a directory.
+
+    Arguments:
+    extension -- [str] File extension without dot to find out
+    directory -- [str] Path to target directory
+
+    Return:
+    filepath -- [str] Path to file found
+    """
+    filelist = []
+    for root, dirnames, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.lower().endswith('.' + extension):
+                filelist.append(os.path.join(root, filename))
+    return filelist
+
 if not len(sys.argv) == 2:
     print("this program requires 1 argument (filepath)")
     exit()
 
 root_dir = sys.argv[1]
 files = []
-for root_dir, dirs, files in os.walk(root_dir):
-    continue
- 
+files = walk_files_with('pickle',root_dir)
+print(files)
 for f in files:
-    with open(root_dir + f, mode='rb') as F:
+    with open(f, mode='rb') as F:
         data = pickle.load(F)
         r_v_list = data["results"]["r_v_list"]
         t = data['results']['t']
         for r_v in r_v_list:
-            v = r_v[1]
-            plt.plot(t, v)
-            plt.show()
-
+            if r_v[0] >= 200 and r_v[0] < 300:
+                v = r_v[1]
+                plt.plot(t, v)
+plt.show()
