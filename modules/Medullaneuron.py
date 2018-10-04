36a862e32a57f302c28ecb4fe21d0f7e73252b9c
Hayato-t <tsunoda.gameobra@x.gmobb.jp>
2018-10-04 12:34:01 +0900

    add medulla

--- modules/Medullaneuron.py
+++ modules/Medullaneuron.py
@@ -23,7 +23,7 @@ class Medullaneuron:
         self.cell["soma"].cm = 10
         if self.celltype == "Tm1":
             self.cell["axon"] = neuron.h.Section(name="axon")
-            self.cell["axon"].nseg = 30
+            self.cell["axon"].nseg = 100
             self.cell["axon"].diam = 0.1
             self.cell["axon"].L = 300
             self.cell["axon"].insert("mole")
@@ -67,7 +67,7 @@ class Medullaneuron:
             self.cell["ap_dend"].insert("mole")
         self.cell["soma"].Ra = 100
         self.cell["ap_dend"].Ra = 100
-        self.cell["axon"].Ra = 1000
+        self.cell["axon"].Ra = 10
         self.cell["axon"].connect(self.cell["soma"], 1)
         self.cell["ap_dend"].connect(self.cell["axon"], 1)
 
